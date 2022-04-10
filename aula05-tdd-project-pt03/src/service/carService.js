c9e94f28bb3a6b092edd3e459c9e74a1792b14e0
lguisantos <lucas.guimaraes0412@gmail.com>
2022-04-10 23:01:41 +0100

    final projeto de testes 03

--- aula05-tdd-project-pt03/src/service/carService.js
+++ aula05-tdd-project-pt03/src/service/carService.js
@@ -1,18 +1,21 @@
 const BaseRepository = require('../../repository/base/baseRepository')
+const Tax = require('../entities/tax')
+const Transaction = require('../entities/transaction')
 
 class CarService {
     constructor({ cars }) {
         this.carRepository = new BaseRepository({ file: cars })
+        this.taxexBasedOnAge = Tax.taxesBasedOnAge
+        this.currencyFormat = new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        })
     }
 
-    async getAvailableCar(id) {
-        return this.carRepository.find(id)
-    }
-
-    chooseRandomCar(carCategory){
-        const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds) 
+    chooseRandomCar(carCategory) {
+        const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
         const carId = carCategory.carIds[randomCarIndex]
-        
+
         return carId
     }
 
@@ -23,11 +26,45 @@ class CarService {
         )
     }
 
-    async getAvailableCar(carCategory){
+    async getAvailableCar(carCategory) {
         const carId = this.chooseRandomCar(carCategory)
         const car = await this.carRepository.find(carId)
 
         return car
+    }
+
+    calculateFinalPrice(customer, carCategory, numberOfDays) {
+        const { age } = customer
+        const { price } = carCategory
+        const { then: tax } = this.taxexBasedOnAge.find(tax => age >= tax.from && age <= tax.to)
+
+        const finalPrice = ((tax * price) * (numberOfDays))
+        const formattedPrice = this.currencyFormat.format(finalPrice)
+        return formattedPrice
+    }
+
+    async rent(customer, carCategory, numberOfDays) {
+        const car = await this.getAvailableCar(carCategory)
+        const finalPrince = await this.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+        const today = new Date()
+        today.setDate(today.getDate() + numberOfDays)
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }
+
+        const dueDate = today.toLocaleDateString('pt-br', options)
+        const transaction = new Transaction({
+            customer,
+            dueDate,
+            car,
+            amount: finalPrince,
+
+        })
+
+        return transaction
     }
 }
 
--- aula05-tdd-project-pt03/test/unitTests/carServices.test.js
+++ aula05-tdd-project-pt03/test/unitTests/carServices.test.js
@@ -7,7 +7,7 @@
 const { describe, it, before, beforeEach, afterEach } = require('mocha')
 const CarService = require('../../src/service/carService')
 const { expect } = require('chai')
-
+const Transaction = require('../../src/entities/transaction')
 const { join } = require('path')
 const sinon = require('sinon')
 const carsDatabase = join(__dirname, './../../database', 'cars.json')
@@ -50,7 +50,6 @@ describe('CarService Suite Tests', () => {
 
         expect(result).to.be.lte(data.length).and.be.gte(0)
     })
-
     it('Should choose the first id from carIds in carCategory', () => {
         const carCategory = mocks.validCarCategory
         const carIdIndex = 0
@@ -73,7 +72,6 @@ describe('CarService Suite Tests', () => {
          */
         expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
     })
-
     it('given a carCategory it should be available', async () => {
         const car = mocks.validCar
         const carCategory = Object.create(mocks.validCarCategory) // Cria uma instancia local do objeto
@@ -103,9 +101,65 @@ describe('CarService Suite Tests', () => {
         /**
          * @description Testando a quantidade de vezes que nossa classe é chamada 
          */
+        expect(result).to.be.deep.equal(expected)
         expect(carService.chooseRandomCar.calledOnce).to.be.ok
-        
+
         expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok
+    })
+    it('given a carCategory, customer and numberOfDays it should calculate final amount in real', async () => {
+        const customer = Object.create(mocks.validCustomer)
+        customer.age = 50
+
+        const carCategory = Object.create(mocks.validCarCategory)
+        carCategory.price = 37.6
+
+        const numberOfDays = 5
+
+        const expected = carService.currencyFormat.format(244.40)
+        const result = carService.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+        expect(result).to.be.deep.equal(expected)
+    })
+    it('given a customer and car category it should return a transaction receipt', async () => {
+        const car = mocks.validCar
+        /**
+         * @description Criando uma cópia do objeto, mas modificando o valor de algumas propriedades
+         */
+        const carCategory = {
+            ...mocks.validCarCategory,
+            price: 37.6,
+            carIds: [car.id]
+        }
+
+        const customer = Object.create(mocks.validCustomer)
+        customer.age = 20
+
+        const numberOfDays = 5
+        const dueDate = '10 de novembro de 2020'
+
+        /**
+         * @description Fixando uma data para que ela náo mudo no new Date()
+         *              Desta forma podemos testar as datas evitando que nossos testes quebrem com o passar do tempo
+         */
+        const now = new Date(2020, 10, 5)
+        sandbox.useFakeTimers(now.getTime())
+
+        sandbox.stub(
+            carService.carRepository,
+            carService.carRepository.find.name
+        ).resolves(car)
+
+        const expectedAmount = carService.currencyFormat.format(206.8)
+        const result = await carService.rent(
+            customer, carCategory, numberOfDays
+        )
+        const expected = new Transaction({
+            customer,
+            car,
+            dueDate,
+            amount: expectedAmount
+        })
+
         expect(result).to.be.deep.equal(expected)
     })
 })
